f9668963c96f391fd8375d19424903d10a31edc2
Chloe <page.prologue@gmail.com>
2021-10-15 00:29:58 +0900

    test : 스프링 통합 테스트 추가

--- src/test/java/hello/spring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/spring/service/MemberServiceIntegrationTest.java
@@ -1,24 +1,22 @@
 package hello.spring.service;
 
 import hello.spring.domain.Member;
+import hello.spring.repository.MemberRepository;
 import hello.spring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.*;
 
-class MemberServiceTest {
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
 
-    private MemberService memberService;
-    private MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     void join() {
